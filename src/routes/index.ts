e6b0f69ad134f393b4887e2b9bc430734a12d22c
Dan Gurney <dan@dangurney.net>
2023-05-09 00:01:27 +0100

    reset form after submit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-export default function index() {
+export default function () {
 	return `
 		<!doctype html>
 			<html>
@@ -8,6 +8,13 @@ export default function index() {
 				<title>HTMX Test</title>
 				<script src="https://unpkg.com/htmx.org@1.9.2"></script>
 				<script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+				<style type="text/tailwindcss">
+					@layer utilities {
+						button, button[type="submit"] {
+							@apply bg-blue-500 text-white px-4 py-2 rounded-md;
+						}
+					}
+				</style>
 			</head>
 			<body class="p-8 flex flex-col space-y-8 items-start">
 				<h1 class="text-3xl font-bold text-green-500" id="title">
@@ -16,8 +23,13 @@ export default function index() {
 				<button hx-post="/clicked" hx-swap="outerHTML">
 					Click Me
 				</button>
-				<form hx-post="/update-title" hx-target="#title" class="flex flex-col space-y-4">
-					<input type="text" name="updated-title" placeholder="Pick a new title" required />
+				<form
+					hx-post="/update-title" 
+					hx-target="#title" 
+					hx-on="htmx:afterRequest: this.reset();"
+					class="flex flex-col items-start space-y-4" 
+				>
+					<input type="text" id="udpated-title" name="updated-title" placeholder="Pick a new title" required />
 					<button type="submit">Update</button>
 				</form>
 			</body>
